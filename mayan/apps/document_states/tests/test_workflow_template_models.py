c1f0509d889c3eb8f47c31ec699536a8e7eae52a
Roberto Rosario <roberto.rosario@mayan-edms.com>
2023-09-15 10:08:35 -0400

    Add workflow model preview test
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/document_states/tests/test_workflow_template_models.py
+++ mayan/apps/document_states/tests/test_workflow_template_models.py
@@ -4,8 +4,14 @@ from .mixins.workflow_template_mixins import WorkflowTemplateTestMixin
 
 
 class WorkflowTemplateModelTestCase(WorkflowTemplateTestMixin, BaseTestCase):
-    def test_workflow_template_preview(self):
+    def test_workflow_template_preview_api_url(self):
         self._create_test_workflow_template()
         self.assertTrue(
             self._test_workflow_template.get_api_image_url()
+        )
+
+    def test_workflow_template_preview_generate_image(self):
+        self._create_test_workflow_template()
+        self.assertTrue(
+            self._test_workflow_template.generate_image()
         )
