16d2ea3b59e2619bb565e4ff20b7b91785564066
Roberto Rosario <roberto.rosario@mayan-edms.com>
2023-11-08 20:01:07 -0400

    Fix a help text typo
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/smart_settings/classes.py
+++ mayan/apps/smart_settings/classes.py
@@ -409,7 +409,7 @@ class Setting:
 
     is_overridden.short_description = _('Overridden')
     is_overridden.help_text = _(
-        'Is this settings being overridden by an environment variable?'
+        'Is this setting being overridden by an environment variable?'
     )
 
     def migrate(self):
