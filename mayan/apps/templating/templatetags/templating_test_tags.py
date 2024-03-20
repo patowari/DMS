65a04302aea2128de87245c6880e163c4832a86a
Roberto Rosario <roberto.rosario@mayan-edms.com>
2024-03-20 13:38:42 -0400

    Add test tag import condition
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/templating/templatetags/templating_test_tags.py
+++ mayan/apps/templating/templatetags/templating_test_tags.py
@@ -1,10 +1,15 @@
+from django.conf import settings
 from django.template import Library
-
-from ..tests.literals import TEST_TEMPLATE_TAG_RESULT
 
 register = Library()
 
 
 @register.simple_tag
 def templating_test_tag():
-    return TEST_TEMPLATE_TAG_RESULT
+    if settings.TESTING:
+        # Hidden import.
+        # Required to allow production package to work when tests are
+        # removed.
+        from ..tests.literals import TEST_TEMPLATE_TAG_RESULT
+
+        return TEST_TEMPLATE_TAG_RESULT
